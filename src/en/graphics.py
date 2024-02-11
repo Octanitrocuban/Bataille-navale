2089753be68784b621cbcaeb94260711ea2b15ca
Octanitrocuban <113181068+Octanitrocuban@users.noreply.github.com>
2024-02-11 21:33:23 +0100

    Add files via upload

--- src/en/graphics.py
+++ src/en/graphics.py
@@ -1,33 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-This module countain the graphical functions used to plot the game board.
+Ce module compte les fonctions graphiques utilisées pour tracer le plateau de
+jeu.
 """
 import matplotlib.pyplot as plt
 import numpy as np
 #=============================================================================
 def show_state_ancrage(tablehumain, dicshiphumain, lplaces):
 	"""
-	This function is used to show the different possibles cells where to put
-	a ship.
+	Cette fonction est utilisée pour montrer les différentes cellules
+	possibles où placer un vaisseau.
 
-	Parameters
+	Paramètres
 	----------
 	tablehumain : numpy.ndarray
-		Human plate with rgb chanel to indicate where are its boats.
+		Table de jeu de l'humaine avec les couleurs rvb pour indiquer où sont
+		les bateaux.
 	dicshiphumain : dict
-		Human dictionary where the state of the human boats is recorded.
+		Dictionnaire humain où l’état des bateaux est enregistré.
 	lplaces : list
-		List of two list of strings. Values to put at the x and y axis. It
-		come from the the function create_pos_pl().
+		Liste de deux listes de chaînes de caractères. Correspond aux valeurs
+		à mettre sur les axes x et y. Provient de la fonction create_pos_pl().
 
-	Returns
-	-------
-	None.
+	Retourne
+	--------
+	Rien.
 
 	"""
 	plt.figure(figsize=(8, 8))
 	plt.imshow(tablehumain, zorder=1)
-	# to have the grid lines and the labes on the axes offset
+	# pour avoir les lignes de la grille et les labels sur les axes décalés
 	plt.xticks(range(10), lplaces[1], fontsize=13)
 	plt.yticks(range(10), lplaces[0], fontsize=13)
 	for i in np.arange(0.5, 9.5, 1):
@@ -36,7 +38,7 @@ def show_state_ancrage(tablehumain, dicshiphumain, lplaces):
 
 	for j in list(dicshiphumain.keys()):
 		plt.plot(dicshiphumain[j]['y'][0], dicshiphumain[j]['x'][0],
-			   's', color=dicshiphumain[j]['rgb'], label=j)
+			   's', color=dicshiphumain[j]['rvb'], label=j)
 
 		plt.legend(loc=(1.01, 0.5))
 
@@ -45,68 +47,65 @@ def show_state_ancrage(tablehumain, dicshiphumain, lplaces):
 def show_state_rotation(table_humain, dic_hum, radius, end_cells, indice_boat,
 						lplaces, rempl_ship_hum, position_xy, ship_name):
 	"""
-	This function is used to show the different possibles rotations for the
-	selected ship.
+	Cette fonction est utilisée pour montrer les différentes rotations
+	possibles pour le navire sélectionné.
 
-	Parameters
+	Paramètres
 	----------
 	table_humain : numpy.ndarray
-		Human plate with rgb chanel to indicate where are its boats.
+		Table de jeu de l'humaine avec les couleurs rvb pour indiquer où sont
+		les bateaux.
 	dic_hum : dict
-		Initilized dictionary of the boats.
+		Dictionnaire d'initialisation des bateaux.
 	radius : int
-		Length (in cell) of the boat.
+		Longueur (en cellule) du bateau.
 	end_cells : numpy.ndarray
-		Numpy ndarray listing the positions at the other end of the boat at
-		the i-th rotation.
+		Matrice numpy énumérant les positions à l’autre extrémité du bateau
+		à la i-ième rotation.
 	indice_boat : int
-		Position at which the chosen boat is in list of the boats. It will
-		be used to access to the parameters of this boat.
+		Position à laquelle le bateau choisi est dans la liste des bateaux.
+		Elle sera utilisée pour accéder aux paramètres de ce bateau.
 	lplaces : list
-		List of two list of strings. Values to put at the x and y axis. It
-		come from the the function create_pos_pl().
+		Liste de deux listes de chaînes de caractères. Correspond aux valeurs
+		à mettre sur les axes x et y. Provient de la fonction create_pos_pl().
 	rempl_ship_hum : dict
-		Human dictionary where the state of the human boats is recorded.
+		Dictionnaire humain où l’état des bateaux est enregistré.
 	position_xy : numpy.ndarray
-		Positions of the pivot point used for rotation.
+		Positions du point de pivotement utilisées pour la rotation.
 	ship_name : str
-		Name of the current choosen boat.
+		Nom du bateau choisit.
 
-	Returns
-	-------
-	None.
+	Retourne
+	--------
+	Rien.
 
 	"""
 	plt.figure(figsize=(8*len(end_cells), 9))
 	for i in range(len(end_cells)):
-		# creating a representation for the possible rotations
+		# créer une représentation pour les rotations possibles
 		table_hum_2 = np.copy(table_humain)
-		# it use the pre computed starting and ending cell to avoid collision
-		# with other boat or to get out of the frame
-		xh = np.linspace(position_xy[0], end_cells[i, 0], radius+1,
-						dtype=int)
-
-		yh = np.linspace(position_xy[1], end_cells[i, 1], radius+1,
-						dtype=int)
-
-		table_hum_2[xh, yh, :] = dic_hum['Rgb codes'][indice_boat]
+		# utilise la cellule de départ et de fin pré-calculée pour éviter une
+		# collision avec un autre bateau ou de sortir du cadre
+		xh = np.linspace(position_xy[0], end_cells[i, 0], radius+1, dtype=int)
+		yh = np.linspace(position_xy[1], end_cells[i, 1], radius+1, dtype=int)
+		table_hum_2[xh, yh, :] = dic_hum['Codes rvb'][indice_boat]
 
 		plt.subplot(1, len(end_cells), i+1)
 		plt.title("Rotation n°"+str(i))
 		plt.imshow(table_hum_2, zorder=1)
-		# plot trick to have a legend with the boat color and name
+		# astuce pour avoir une légende avec la couleur et le nom du bateau
 		plt.plot(yh[0], xh[0], 's', label=ship_name,
-				color=dic_hum['Rgb codes'][indice_boat], zorder=2)
+				color=dic_hum['Codes rvb'][indice_boat], zorder=2)
 
-		# plot the already placed boats
+		# afficher les bateaux déjà placés
 		for j in list(rempl_ship_hum.keys()):
 			plt.plot(rempl_ship_hum[j]['y'][0], rempl_ship_hum[j]['x'][0],
-					 's', color=rempl_ship_hum[j]['rgb'], label=j, zorder=2)
+					 's', color=rempl_ship_hum[j]['rvb'], label=j, zorder=2)
 
 		plt.plot(position_xy[1], position_xy[0], "ro",
-				 label="Attachment\npoint", zorder=2)
+				 label="Centre de\nrotation", zorder=2)
 
-		# to have the grid lines and the labes on the axes offset
+		# pour avoir les lignes de grille et les labels sur les axes décalés
 		plt.xticks(range(10), lplaces[1], fontsize=13)
 		plt.yticks(range(10), lplaces[0], fontsize=13)
 		for j in np.arange(0.5, 9.5, 1):
@@ -122,44 +121,50 @@ def show_comp_comp(table_bot1, dico_bot1, watter_bot1, notsink_bot1,
 				   sinked_bot1, table_bot2, dico_bot2, watter_bot2,
 				   notsink_bot2, sinked_bot2, list_ppl):
 	"""
-	Function to show the fight between two bots.
+	Fonction pour montrer la partie en cours entre deux bots.
 
-	Parameters
+	Paramètres
 	----------
 	table_bot1 : numpy.ndarray
-		Plate of the first bot with rgb chanel to indicate where are its
-		boats.
+		Table de jeu du premier bot avec les couleurs rvb pour indiquer
+		où sont les bateaux.
 	dico_bot1 : dict
-		First bot dictionary where the state of the boats is recorded.
+		Dictionnaire du premier bot où l’état des bateaux est enregistré.
 	watter_bot1 : list
-		List of hit that end in the see made by bot 1.
+		Liste des tirs du premier bot qui ont touché une cellule contenant
+		de l'eau.
 	notsink_bot1 : list
-		List of hit that touch a boat but without sinking it by bot 1.
+		Liste des tirs du premier bot qui ont touché une cellule contenant
+		un bateau sans le couler.
 	sinked_bot1 : list
-		List of hit that touch a boat and sinked it by bot 1.
+		Liste des tirs du premier bot qui ont touché une cellule contenant
+		un bateau qui a été coulé.
 	table_bot2 : numpy.ndarray
-		Plate of the second bot with rgb chanel to indicate where are its
-		boats.
+		Table de jeu du second bot avec les couleurs rvb pour indiquer
+		où sont les bateaux.
 	dico_bot2 : dict
-		Second bot dictionary where the state of the boats is recorded.
+		Dictionnaire du second bot où l’état des bateaux est enregistré.
 	watter_bot2 : list
-		List of hit that end in the see made by bot 2.
+		Liste des tirs du second bot qui ont touché une cellule contenant
+		de l'eau.
 	notsink_bot2 : list
-		List of hit that touch a boat but without sinking it by bot 2.
+		Liste des tirs du second bot qui ont touché une cellule contenant
+		un bateau sans le couler.
 	sinked_bot2 : list
-		List of hit that touch a boat and sinked it by bot 2.
+		Liste des tirs du second bot qui ont touché une cellule contenant
+		un bateau qui a été coulé.
 	list_ppl : list
-		List of two list of strings. Values to put at the x and y axis. It
-		come from the the function create_pos_pl().
+		Liste de deux listes de chaînes de caractères. Correspond aux valeurs
+		à mettre sur les axes x et y. Provient de la fonction create_pos_pl().
 
-	Returns
-	-------
-	None.
+	Retourne
+	--------
+	Rien.
 
 	"""
 	plt.figure(figsize=(13, 5))
 	plt.subplot(1, 2, 1)
-	plt.title("Player 1 plate")
+	plt.title("Grille du joueur 1")
 	plt.imshow(table_bot1, zorder=1)
 	plt.xticks(range(10), list_ppl[1], fontsize=13)
 	plt.yticks(range(10), list_ppl[0], fontsize=13)
@@ -169,7 +174,7 @@ def show_comp_comp(table_bot1, dico_bot1, watter_bot1, notsink_bot1,
 
 	for j in list(dico_bot1.keys()):
 		plt.plot(dico_bot1[j]['y'][0], dico_bot1[j]['x'][0], 's',
-				 color=dico_bot1[j]['rgb'], label=j, zorder=2)
+				 color=dico_bot1[j]['rvb'], label=j, zorder=2)
 
 	for j in range(len(watter_bot1)):
 		plt.plot(watter_bot1[j][1], watter_bot1[j][0], 'bo', zorder=2)
@@ -182,7 +187,7 @@ def show_comp_comp(table_bot1, dico_bot1, watter_bot1, notsink_bot1,
 
 	plt.legend(loc=(1.01, 0.5))
 	plt.subplot(1, 2, 2)
-	plt.title("Player 2 plate")
+	plt.title("Grille du joueur 2")
 	plt.imshow(table_bot2, zorder=1)
 	plt.xticks(range(10), list_ppl[1], fontsize=13)
 	plt.yticks(range(10), list_ppl[0], fontsize=13)
@@ -192,7 +197,7 @@ def show_comp_comp(table_bot1, dico_bot1, watter_bot1, notsink_bot1,
 
 	for j in list(dico_bot2.keys()):
 		plt.plot(dico_bot2[j]['y'][0], dico_bot2[j]['x'][0],
-			   's', color=dico_bot2[j]['rgb'], label=j, zorder=2)
+			   's', color=dico_bot2[j]['rvb'], label=j, zorder=2)
 
 	for j in range(len(watter_bot2)):
 		plt.plot(watter_bot2[j][1], watter_bot2[j][0], 'bo', zorder=2)
@@ -209,53 +214,60 @@ def show_comp_comp(table_bot1, dico_bot1, watter_bot1, notsink_bot1,
 def show_state_of_the_fight(table_humain, listppl, human_dico, ordi_watter,
 							ordi_notsink, ordi_sinked, what_human_see):
 	"""
-	Function to show the human and computer grid during the fight. For
-	the human grid, there will be:
-		-its ship's position as colored cells;
-		-the sea in white cells;
-		-the legend with the color corresponding to the remaing ships;
-		-the positions where the computer hit the sea with blue dots;
-		-the positions where the computer hit a baot with red dots;
-		-the positions where the computer sinked a baot with dark dots;
+	Fonction pour montrer les grille de l'humaine et de l'ordinateur pendant
+	le match. Pour la grille humaine, il y aura :
+		-les positions de ses bateaux indiqué par la couleurs des cellules;
+		-La mer est coloriée en blanc;
+		-la légende avec la couleur correspondant aux navires restant;
+		-les positions où l’ordinateur a frappé la mer avec des points bleus;
+		-les positions où l’ordinateur a frappé un bateau avec des points rouges;
+		-les positions où l’ordinateur a coulé un bateau avec des points noirs;
 
-	Parameters
+	Paramètres
 	----------
 	table_humain : numpy.ndarray
-		Human plate with rgb chanel to indicate where are its boats.
-	list_ppl : list
-		List of two list of strings. Values to put at the x and y axis. It
-		come from the the function create_pos_pl().
+		Table de jeu de l'humaine avec les couleurs rvb pour indiquer où sont
+		les bateaux.
+	listppl : list
+		Liste de deux listes de chaînes de caractères. Correspond aux valeurs
+		à mettre sur les axes x et y. Provient de la fonction create_pos_pl().
 	human_dico : dict
-		Human dictionary where the state of the human boats is recorded.
+		Dictionnaire humain où l’état des bateaux est enregistré.
 	ordi_watter : list
-		List of hit that end in the see made by computer.
+		Liste des tirs de l'ordinateur qui ont touché une cellule contenant
+		de l'eau.
 	ordi_notsink : list
-		List of hit that touch a boat but without sinking it by computer.
+		Liste des tirs qui ont touché une cellule contenant un bateau sans
+		le couler.
 	ordi_sinked : list
-		List of hit that touch a boat and sinked it by computer.
+		Liste des tirs qui ont touché une cellule contenant un bateau qui a
+		été coulé.
 	what_human_see : numpy.ndarray
-		What human see from its hits. It is a rgb 2d array with: blue cell
-		indicate that it is watter, red cell indicate that it is a boat not
-		sinked yet and dark cell indicate that it is sinked boat.
+		Table de jeu où l'humain peut voir les conséquences de ses tirs contre
+		l'ordinateur. C’est un tableau 2d rgb avec : les cellules bleue
+		indiquent un coup dans l'eau, les cellules rouge indiquent qu’un
+		bateau a été touché mais pas encore coulé, les cellules noires
+		indiquent qu’un bateau a été touché et coulé et les cellules blanche
+		sont celles qui n'ont pas encore été explorées.
 
-	Returns
-	-------
-	None.
+	Retourne
+	--------
+	Rien.
 
 	"""
 	plt.figure(figsize=(19, 9))
 	plt.subplot(1, 2, 1)
-	plt.title("Your plate")
+	plt.title("Table de jeu de l'humain")
 	plt.imshow(table_humain, zorder=1)
-	plt.xticks(range(10), list_ppl[1], fontsize=13)
-	plt.yticks(range(10), list_ppl[0], fontsize=13)
+	plt.xticks(range(10), listppl[1], fontsize=13)
+	plt.yticks(range(10), listppl[0], fontsize=13)
 	for i in np.arange(0.5, 9.5, 1):
 		plt.hlines(i, -0.5, 9.5, "k", zorder=2)
 		plt.vlines(i, -0.5, 9.5, "k", zorder=2)
 
 	for j in list(human_dico.keys()):
 		plt.plot(human_dico[j]['y'][0], human_dico[j]['x'][0],
-			   's', color=human_dico[j]['rgb'], label=j)
+			   's', color=human_dico[j]['rvb'], label=j)
 
 	for j in range(len(ordi_watter)):
 		plt.plot(ordi_watter[j][1], ordi_watter[j][0], 'bo')
@@ -268,10 +280,10 @@ def show_state_of_the_fight(table_humain, listppl, human_dico, ordi_watter,
 
 	plt.legend(loc=(1.01, 0.5))
 	plt.subplot(1, 2, 2)
-	plt.title("Computer plate")
+	plt.title("Table de jeu de l'orninateur")
 	plt.imshow(what_human_see, zorder=1)
-	plt.xticks(range(10), list_ppl[1], fontsize=13)
-	plt.yticks(range(10), list_ppl[0], fontsize=13)
+	plt.xticks(range(10), listppl[1], fontsize=13)
+	plt.yticks(range(10), listppl[0], fontsize=13)
 	for i in np.arange(0.5, 9.5, 1):
 		plt.hlines(i, -0.5, 9.5, "k", zorder=2)
 		plt.vlines(i, -0.5, 9.5, "k", zorder=2)
@@ -280,28 +292,28 @@ def show_state_of_the_fight(table_humain, listppl, human_dico, ordi_watter,
 
 def print_victory(victoire, list_sinked, list_water):
 	"""
-	Function to print the result of a game.
+	Fonction pour afficher le résultat d'un match.
 
-	Parameters
+	Paramètres
 	----------
 	victoire : str
-		String that indicates who win the match.
+		Chaîne de caractères indiquant qui a remporté le match.
 	list_sinked : list
-		List of the hit that had touch water cell.
+		Liste des tirs qui ont touché une cellule contenant un bateau.
 	list_water : list
-		List of the hit that had touch boat cell.
+		Liste des tirs qui ont touché une cellule contenant de l'eau.
 
-	Returns
-	-------
+	Retourne
+	--------
 	int
-		Number of hit during the match.
+		Nombre de coup réaliser par le joueur durant le match.
 
 	"""
-	if victoire == "null":
-		print("Draw")
+	if victoire == "nulle":
+		print("Partie nulle.")
 
-	elif victoire == "human":
-		print("Human victory")
+	elif victoire == "humain":
+		print("Victoire humain.")
 		print("******    ******     ******     *           *    ******* ")
 		print("*     *   *     *   *       *    *         *    *       *")
 		print("*     *   *     *   *       *     *       *     *       *")
@@ -310,65 +322,65 @@ def print_victory(victoire, list_sinked, list_water):
 		print("*     *   *   *     *       *        * *        *       *")
 		print("******    *     *   *       *         *          ******* ")
 	
-	elif victoire == "computer":
-		print("Computer victory")
+	elif victoire == "ordinateur":
+		print("Victoire ordinateur.")
 
 	return len(list_sinked)+len(list_water)
 
 def pretty_dict_print(ship_dico):
 	"""
-	Function to have a nice print of the state of your boat when placing
-	them.
+	Fonction pour avoir un bel affichage de l’état des bateau durant de
+	leur placement sur le plateau de jeu.
 
-	Parameters
+	Paramètres
 	----------
 	ship_dico : dict
-		Dictionary where the state of the boats is recorded.
+		Dictionnaire où l’état des bateaux est enregistré.
 
-	Returns
-	-------
-	None.
+	Retourne
+	--------
+	Rien.
 
 	"""
-	num = ship_dico['Number']
+	num = ship_dico['Nombre']
 	nam = ship_dico['Type']
-	sep = '+'+'-'*18+'+'+'-'*12+'+'+'-'*8+'+'+'-'*8+'+'+'-'*9+'+'+'-'*20+'+'
+	sep = '+'+'-'*13+'+'+'-'*9+'+'+'-'*10+'+'+'-'*8+'+'+'-'*9+'+'+'-'*20+'+'
 	print(sep)
-	print('|       Type       | Identifier | Length | Number |  Color  |'
-		 +'     Rgb codes      |')
+	print('|     Type    | Numéros | Longueur | Nombre | Couleur |'
+		 +'      Codes rvb     |')
 
 	print(sep)
-	if 'Aircraft-carrier' in ship_dico['Type']:
-		print('| Aircraft-carrier |      0     |   5    |    '
-			 +str(num[nam == 'Aircraft-carrier'][0])+'   |  grey   | '
+	if 'Porte-avion' in ship_dico['Type']:
+		print('| Porte-avion |    0    |     5    |    '
+			 +str(num[nam == 'Porte-avion'][0])+'   |  gris   | '
 			 +'[0.50, 0.50, 0.50] |')
 
 		print(sep)
 
-	if 'Cruiser' in ship_dico['Type']:
-		print('|      Cruiser     |      1     |   4    |    '
-			 +str(num[nam == 'Cruiser'][0])+'   | orange  | '
+	if 'Croiseur' in ship_dico['Type']:
+		print('|   Croiseur  |    1    |     4    |    '
+			 +str(num[nam == 'Croiseur'][0])+'   | orange  | '
 			 +'[1.00, 0.50, 0.00] |')
 
 		print(sep)
 
-	if 'Submarine' in ship_dico['Type']:
-		print('|     Submarine    |      2     |   3    |    '
-			 +str(num[nam == 'Submarine'][0])+'   |  green  | '
+	if 'Sous-marin' in ship_dico['Type']:
+		print('|  Sous-marin |    2    |     3    |    '
+			 +str(num[nam == 'Sous-marin'][0])+'   |   vert  | '
 			 +'[0.00, 0.75, 0.00] |')
 
 		print(sep)
 
-	if 'Frigate' in ship_dico['Type']:
-		print('|      Frigate     |      3     |   3    |    '
-			 +str(num[nam == 'Frigate'][0])+'   | yellow  | '
+	if 'Frégate' in ship_dico['Type']:
+		print('|    Frégate  |    3    |     3    |    '
+			 +str(num[nam == 'Frégate'][0])+'   |  jaune  | '
 			 +'[1.00, 0.90, 0.00] |')
 
 		print(sep)
 
-	if 'Shuttle' in ship_dico['Type']:
-		print('|      Shuttle     |      4     |   2    |    '
-			 +str(num[nam == 'Shuttle'][0])+'   | purple  | '
+	if 'Navette' in ship_dico['Type']:
+		print('|    Navette  |    4    |     2    |    '
+			 +str(num[nam == 'Navette'][0])+'   | violet  | '
 			 +'[0.75, 0.00, 0.75] |')
 
 		print(sep)
